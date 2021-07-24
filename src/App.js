04c3813e9cc69f194b660847936922095e9ca301
Pratap Tyagi <tyagipratap111@gmail.com>
2021-07-24 22:36:35 +0530

    Adds github pages deployment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { Navbar } from "./components";
 import Routing from "./Routing";
 import "./App.css";
